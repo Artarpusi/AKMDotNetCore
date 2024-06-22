0ea106ed34799dd6e7a71e6cb60ecee75a0ab118
ArKarMin <arkarminbtw@gmail.com>
2024-06-22 12:29:52 +0630

    Minimal Api

---
+++ AKMDotNetCore.MinimalApi/Db/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AKMDotNetCore.MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKMDotNetCore.MinimalApi.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        //}
+
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
